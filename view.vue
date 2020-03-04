f0e2b1756540644e2353c703d9e825b81a887d81
Ascellus <marljohn.ifliba@gmail.com>
2020-03-04 09:46:34 +0800

    Initial Commit

---
+++ view.vue
@@ -0,0 +1,20 @@
+
+new Vue({
+            el: '#app',
+            data: {
+                'message': 'This is world'
+            }
+            
+});
+
+
+
+new Vue({
+            el: '#appd',
+            data: {
+                'messages': 'Hello Jeremy!'
+            }
+});
+
+
+
